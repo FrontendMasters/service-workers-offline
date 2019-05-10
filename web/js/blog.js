2cb355730a88a817f3939578d162dc433acd1fee
Kyle Simpson <getify@gmail.com>
2019-05-09 19:48:56 -0500

    adding sw

--- web/js/blog.js
+++ web/js/blog.js
@@ -4,6 +4,13 @@
 	var offlineIcon;
 	var isOnline = ("onLine" in navigator) && navigator.onLine;
 	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+	var usingSW = ("serviceWorker" in navigator);
+	var swRegistration;
+	var svcworker;
+
+	if (usingSW) {
+		initServiceWorker().catch(console.error);
+	}
 
 	document.addEventListener("DOMContentLoaded",ready,false);
 
@@ -25,6 +32,19 @@
 			offlineIcon.classList.remove("hidden");
 			isOnline = false;
 		},false);
+	}
+
+	async function initServiceWorker() {
+		swRegistration = await navigator.serviceWorker.register("/sw.js",{
+			updateViaCache: "none",
+		});
+
+		svcworker = swRegistration.installing || swRegistration.waiting || swRegistration.active;
+
+		// listen for new service worker to take over
+		navigator.serviceWorker.addEventListener("controllerchange",async function onController(){
+			svcworker = navigator.serviceWorker.controller;
+		});
 	}
 
 })();
--- web/js/sw.js
+++ web/js/sw.js
@@ -1,3 +1,29 @@
 "use strict";
 
-// TODO
+var version = 1;
+
+self.addEventListener("install",onInstall);
+self.addEventListener("activate",onActivate);
+
+main().catch(console.error);
+
+
+// ****************************
+
+async function main() {
+	console.log(`Service Worker (v${version}) started`);
+}
+
+function onInstall(evt) {
+	console.log(`Service Worker (v${version}) installed`);
+	self.skipWaiting();
+}
+
+function onActivate(evt) {
+	evt.waitUntil(handleActivation());
+}
+
+async function handleActivation() {
+	await clients.claim();
+	console.log(`Service Worker (v${version}) activated`);
+}
