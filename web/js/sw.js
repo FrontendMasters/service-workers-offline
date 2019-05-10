6800ea5ab64fbae460b5d7910d29d16c9bceda76
Kyle Simpson <getify@gmail.com>
2019-05-09 20:06:20 -0500

    sw: caching logged out files

--- web/js/sw.js
+++ web/js/sw.js
@@ -1,8 +1,27 @@
 "use strict";
 
-var version = 2;
+var version = 3;
 var isOnline = true;
 var isLoggedIn = false;
+var cacheName = `ramblings-${version}`;
+
+var urlsToCache = {
+	loggedOut: [
+		"/",
+		"/about",
+		"/contact",
+		"/404",
+		"/login",
+		"/offline",
+		"/css/style.css",
+		"/js/blog.js",
+		"/js/home.js",
+		"/js/login.js",
+		"/js/add-post.js",
+		"/images/logo.gif",
+		"/images/offline.png"
+	]
+};
 
 self.addEventListener("install",onInstall);
 self.addEventListener("activate",onActivate);
@@ -15,6 +34,7 @@ main().catch(console.error);
 
 async function main() {
 	await sendMessage({ statusUpdateRequest: true });
+	await cacheLoggedOutFiles();
 }
 
 function onInstall(evt) {
@@ -27,8 +47,39 @@ function onActivate(evt) {
 }
 
 async function handleActivation() {
+	await cacheLoggedOutFiles(/*forceReload=*/true);
 	await clients.claim();
 	console.log(`Service Worker (v${version}) activated`);
+}
+
+async function cacheLoggedOutFiles(forceReload = false) {
+	var cache = await caches.open(cacheName);
+
+	return Promise.all(
+		urlsToCache.loggedOut.map(async function requestFile(url){
+			try {
+				let res;
+
+				if (!forceReload) {
+					res = await cache.match(url);
+					if (res) {
+						return;
+					}
+				}
+
+				let fetchOptions = {
+					method: "GET",
+					cache: "no-store",
+					credentials: "omit"
+				};
+				res = await fetch(url,fetchOptions);
+				if (res.ok) {
+					return cache.put(url,res);
+				}
+			}
+			catch (err) {}
+		})
+	);
 }
 
 async function sendMessage(msg) {
