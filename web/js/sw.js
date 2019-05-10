46aeeadbbc4a85d8debac1ed2d04d23a67c9941b
Kyle Simpson <getify@gmail.com>
2019-05-10 07:44:15 -0500

    sw: caching all posts in the background

--- web/js/sw.js
+++ web/js/sw.js
@@ -1,9 +1,10 @@
 "use strict";
 
-var version = 6;
+var version = 7;
 var isOnline = true;
 var isLoggedIn = false;
 var cacheName = `ramblings-${version}`;
+var allPostsCaching = false;
 
 var urlsToCache = {
 	loggedOut: [
@@ -36,6 +37,7 @@ main().catch(console.error);
 async function main() {
 	await sendMessage({ statusUpdateRequest: true });
 	await cacheLoggedOutFiles();
+	return cacheAllPosts();
 }
 
 function onInstall(evt) {
@@ -52,6 +54,9 @@ async function handleActivation() {
 	await cacheLoggedOutFiles(/*forceReload=*/true);
 	await clients.claim();
 	console.log(`Service Worker (v${version}) activated`);
+
+	// spin off background caching of all past posts (over time)
+	cacheAllPosts(/*forceReload=*/true).catch(console.error);
 }
 
 async function clearCaches() {
@@ -99,6 +104,102 @@ async function cacheLoggedOutFiles(forceReload = false) {
 			catch (err) {}
 		})
 	);
+}
+
+async function cacheAllPosts(forceReload = false) {
+	// already caching the posts?
+	if (allPostsCaching) {
+		return;
+	}
+	allPostsCaching = true;
+	await delay(5000);
+
+	var cache = await caches.open(cacheName);
+	var postIDs;
+
+	try {
+		if (isOnline) {
+			let fetchOptions = {
+				method: "GET",
+				cache: "no-store",
+				credentials: "omit"
+			};
+			let res = await fetch("/api/get-posts",fetchOptions);
+			if (res && res.ok) {
+				await cache.put("/api/get-posts",res.clone());
+				postIDs = await res.json();
+			}
+		}
+		else {
+			let res = await cache.match("/api/get-posts");
+			if (res) {
+				let resCopy = res.clone();
+				postIDs = await res.json();
+			}
+			// caching not started, try to start again (later)
+			else {
+				allPostsCaching = false;
+				return cacheAllPosts(forceReload);
+			}
+		}
+	}
+	catch (err) {
+		console.error(err);
+	}
+
+	if (postIDs && postIDs.length > 0) {
+		return cachePost(postIDs.shift());
+	}
+	else {
+		allPostsCaching = false;
+	}
+
+
+	// *************************
+
+	async function cachePost(postID) {
+		var postURL = `/post/${postID}`;
+		var needCaching = true;
+
+		if (!forceReload) {
+			let res = await cache.match(postURL);
+			if (res) {
+				needCaching = false;
+			}
+		}
+
+		if (needCaching) {
+			await delay(10000);
+			if (isOnline) {
+				try {
+					let fetchOptions = {
+						method: "GET",
+						cache: "no-store",
+						credentials: "omit"
+					};
+					let res = await fetch(postURL,fetchOptions);
+					if (res && res.ok) {
+						await cache.put(postURL,res.clone());
+						needCaching = false;
+					}
+				}
+				catch (err) {}
+			}
+
+			// failed, try caching this post again?
+			if (needCaching) {
+				return cachePost(postID);
+			}
+		}
+
+		// any more posts to cache?
+		if (postIDs.length > 0) {
+			return cachePost(postIDs.shift());
+		}
+		else {
+			allPostsCaching = false;
+		}
+	}
 }
 
 async function sendMessage(msg) {
